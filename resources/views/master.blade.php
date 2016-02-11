461df0c36b3936c3a931e402ba1884311f100ac8
Gunharth Randolf <hello@gunharth.io>
2016-02-11 17:59:08 +0100

    Added viewport meta

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
+	<meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>ReelsApp</title>
 	<!-- Fonts -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
